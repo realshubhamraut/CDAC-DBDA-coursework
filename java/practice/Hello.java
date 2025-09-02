ab470098f0933ef10b6d680aa7ed5bca7c67f068
Shubham Raut <textnow5123@gmail.com>
2025-09-02 17:09:48 +0530

    updated the linux assignments for the 2nd and first part

--- java/practice/Hello.java
+++ java/practice/Hello.java
@@ -1,3 +1,18 @@
-class Learningjava{
-    
+class Hello{
+    public static void main(String[] args) {
+        int salcal;
+        
+        int sal = 100;
+        String str = "Manoj";
+        System.out.println("Hi");
+        System.out.println("hi");
+        System.out.println("new");
+        System.out.println(sal);
+        System.out.println(str.hashCode());
+        int mob = 13451351;
+        long mob1 = 123;
+
+
+
+     }
 }
---
+++ java/practice/Variables.java
@@ -0,0 +1,14 @@
+public class Variables{
+    public static void main(String args[]) {
+        double price = 19.99;
+        int age = 12;
+        String name = "Alice";
+        boolean isJavaFun = true;
+
+        System.out.println("Name: " +name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: "+ price);
+        System.out.println("Is java fun?: "+ isJavaFun);
+        
+    }
+}
