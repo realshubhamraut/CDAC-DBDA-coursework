3cb517da16c5e82f10643a9b9e0dea37025e4c18
Shubham Raut <textnow5123@gmail.com>
2025-09-03 14:28:37 +0530

    updated the java assignment

---
+++ java/practice/Age.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class Age {
+    public static void main(String[] args){
+        System.out.println("Hi, please put your age: ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println("Age of the user is: " + i);
+
+
+    }
+    
+}
+
---
+++ java/practice/WrapperClassStudy.java
@@ -0,0 +1,9 @@
+public class WrapperClassStudy {
+    static int outside; 
+    static float other;
+    public static void main(String args[]) {
+    System.out.println(outside);
+    System.out.println(other);
+    } 
+    
+}
