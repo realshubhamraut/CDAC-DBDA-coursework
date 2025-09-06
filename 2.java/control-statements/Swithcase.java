7cf588884284dc86a06815eebc4da94f38c507ca
Shubham Raut <textnow5123@gmail.com>
2025-09-06 23:42:23 +0530

    new codes added

---
+++ 2.java/control-statements/Swithcase.java
@@ -0,0 +1,34 @@
+public class Swithcase {
+    public static void main(String args[]){
+        int day;
+        Scanner sc = new Scanner(System.in);
+        
+        switch(day){
+            case 1:
+            System.out.println("Monday");
+            break;
+
+            case 2:
+            System.out.println("Tuesday");
+
+            case 3:
+            System.out.println("Wednesday");
+
+            case 4:
+            System.out.println("Thursday");
+
+            case 5:
+            System.out.println("Friday");
+
+            case 6:
+            System.out.println("Saturday");
+
+            case 7:
+            System.out.println("Sunday");
+            
+            default:
+            System.out.println("invalid day");
+        }
+    }
+    
+}
--- 2.java/practice/Assignment.java
+++ 2.java/practice/Assignment.java
@@ -13,11 +13,71 @@
 // Display who is getting highest salary for that month and what amount?
 // use this to refer to current objcet.
 
-public class Assignment {
+class EmployeeManagement {
     int id;
     String name;
-    String salary;
-    String holdiays;
+    int holidays;
     String address;
-    
+    int salary;
+
+    EmployeeManagement(int id, String name, int holidays, String address) {
+        this.id = id;
+        this.name = name;
+        this.holidays = holidays;
+        this.address = address;
+    }
+
+    int calculateSalary(int daysWorked, int dailySalary) {
+        this.salary = daysWorked * dailySalary;
+        return this.salary;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary + ", Address: " + address);
+    }
 }
+
+class Manager extends EmployeeManagement {
+    static final int DAILY_SALARY = 500;
+    Manager(int id, String name, int holidays, String address) {
+        super(id, name, holidays, address);
+    }
+}
+
+class Peon extends EmployeeManagement {
+    static final int DAILY_SALARY = 100;
+    Peon(int id, String name, int holidays, String address) {
+        super(id, name, holidays, address);
+    }
+}
+
+class NewJoinee extends EmployeeManagement {
+    static final int DAILY_SALARY = 200;
+    NewJoinee(int id, String name, int holidays, String address) {
+        super(id, name, holidays, address);
+    }
+}
+
+public class Assignment {
+    public static void main(String[] args) {
+        int totalDays = 30;
+
+        Manager m = new Manager(1, "Amit", 2, "Pune");
+        Peon p = new Peon(2, "Sumit", 1, "Mumbai");
+        NewJoinee n = new NewJoinee(3, "Ravi", 0, "Delhi");
+
+        int mSalary = m.calculateSalary(totalDays - m.holidays, Manager.DAILY_SALARY);
+        int pSalary = p.calculateSalary(totalDays - p.holidays, Peon.DAILY_SALARY);
+        int nSalary = n.calculateSalary(totalDays - n.holidays, NewJoinee.DAILY_SALARY);
+
+        m.display();
+        p.display();
+        n.display();
+
+        // Find highest salary
+        EmployeeManagement highest = m;
+        if (p.salary > highest.salary) highest = p;
+        if (n.salary > highest.salary) highest = n;
+
+        System.out.println("Highest salary: " + highest.name + " with amount " + highest.salary);
+    }
