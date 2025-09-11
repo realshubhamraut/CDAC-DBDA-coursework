0e5c2bca67ab63298b3685b752fdc65a24036da5
Shubham Raut <textnow5123@gmail.com>
2025-09-11 21:35:28 +0530

    updated with the recent assignment examples

---
+++ 2.java/assignments/assignment-2/Ques21.java
@@ -0,0 +1,21 @@
+// Create an abstract class Vehicle with an abstract method start (). Create subclasses
+// Car and Bike that provide their own implementation of start ().
+
+abstract class Vehicle{
+    abstract void start();
+}
+
+class Bike extends Vehicle{
+    void start(){
+    }
+}
+
+class Car extends Vehicle{
+    void start(){
+    }
+}
+
+public class Ques21 {
+    public static void main(String[] args){
+    }
+}
