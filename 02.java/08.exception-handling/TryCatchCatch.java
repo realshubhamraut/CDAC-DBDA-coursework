cf5a0c71f4c73ded5cca1943ae1460c22fa54462
Shubham Raut <textnow5123@gmail.com>
2025-09-12 20:55:04 +0530

    updated with the assignments

--- 02.java/08.exception-handling/TryCatchCatch.java
+++ 02.java/08.exception-handling/TryCatchCatch.java
@@ -1,15 +1,34 @@
 // handling multiple exceptions;
 
+import java.util.*;
+
+
 public class TryCatchCatch{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         try{
-            int[] arr = {1,2,12,4,1};
-            System.out.println(arr[23]);
-        } catch (ArrayIndexOutOfBoundsException rt) {
-            System.out.println("Array out of call");
-        } catch (ArithmeticException ae){
-            System.out.println("Division error/ cannot divide by zero");
+            int whichIndex;
+            int[] arr = {123,141,45,112,12};
+            System.out.print("Tell me the index you wanna access: ");
+            whichIndex = sc.nextInt();
+            System.out.print("Value at index: "+ whichIndex + ": " + arr[whichIndex]);
+            
+        } catch (ArrayIndexOutOfBoundsException exind){
+            System.out.println("index out of scope for the array");
         }
-        System.out.println("Program executed safely, no worries");
+
+        try{
+            System.out.print("Enter the number 1: ");
+            int a = sc.nextInt();
+            System.out.print("Enter the number 2: ");
+            int b = sc.nextInt();
+            int result = a / b;
+            System.out.println("result:" + result); 
+        } catch(ArithmeticException ae){
+            System.out.println("arithmetic exception");
+        }
+        System.out.println("No errors detected, program moved ahead");
+        sc.close();
     }
 }
+
