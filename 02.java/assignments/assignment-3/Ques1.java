3491c309d67a078161f22c2750a9f8db7150d0a5
Shubham Raut <textnow5123@gmail.com>
2025-09-12 11:29:46 +0530

    updated the folder with the new names

---
+++ 02.java/assignments/assignment-3/Ques1.java
@@ -0,0 +1,18 @@
+// You are developing a Java program for a simple library management system using an ArrayList
+// to manage books in the library. Each book should have attributes including the title, author, and
+// ISBN number. Implement a Book class with methods to perform the following actions:
+// I. Allow users to add a new book to the library.
+// 2. Allow users to remove a book from the library by providing its ISBN number.
+// 3. Enable users to search for a book by title and display its details.
+// 4. Display the list of all books currently in the library.
+
+
+
+public class Library{
+    String title;
+    String author;
+    int isbn;
+}
+public class Ques1 {
+    
+}
