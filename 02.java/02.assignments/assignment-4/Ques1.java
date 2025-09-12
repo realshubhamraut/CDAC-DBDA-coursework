ef1d6b40b9f5912c4b02adbf24da8f61a4a17f9c
Shubham Raut <textnow5123@gmail.com>
2025-09-12 15:26:33 +0530

    updated the code

--- 02.java/02.assignments/assignment-4/Ques1.java
+++ 02.java/02.assignments/assignment-4/Ques1.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+
 public class Ques1 {
-    
+    public static void main(String[] args) {
+        // Create a new ArrayList to store movie names
+        ArrayList<String> movies = new ArrayList<>();
+
+        // Add some movie names
+        movies.add("Inception");
+        movies.add("The Matrix");
+        movies.add("Interstellar");
+        movies.add("The Dark Knight");
+
+        // Print the collection
+        System.out.println("Movie list: " + movies);
+
+        // Insert a movie at the first position (index 0)
+        movies.add(0, "Shawshank Redemption");
+
+        // Print the updated collection
+        System.out.println("After inserting at first position: " + movies);
+    }
 }
