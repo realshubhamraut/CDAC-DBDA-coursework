a76c2cb7d021c503a65d9b35fe2378aa4bfc8c65
Shubham Raut <textnow5123@gmail.com>
2025-09-13 15:48:26 +0530

    updated the assignment files

---
+++ 02.java/02.assignments/assignment-1/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+26. Write a program to find minimum and maximum number as well as
+// to add two float numbers using methods of Float.
--- Try.java
+++ Try.java
@@ -1,14 +1,1 @@
-public class Try{
-    public static void main(String[] args){
-        try{
-            int a = 10;
-            int b = 12;
-            int result = a/b;
-            System.out.println("Result" + result);
 
-        } catch (ArithmeticException ae){
-            System.out.println("Zero division error");
-        }
-        System.out.println("Program continues....");
-    }
-}
