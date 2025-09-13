c98ca4f07f74dc084f45c9aef172036ae5d91204
Shubham Raut <textnow5123@gmail.com>
2025-09-13 09:05:40 +0530

    added assignemnt 4&5

--- Try.java
+++ Try.java
@@ -2,13 +2,13 @@ public class Try{
     public static void main(String[] args){
         try{
             int a = 10;
-            int b = 0;
-            int result = a /b;
-            System.out.println("Result: " + result);
+            int b = 12;
+            int result = a/b;
+            System.out.println("Result" + result);
 
         } catch (ArithmeticException ae){
-            System.out.println("cannot divide by zero/ zero divsion error");
+            System.out.println("Zero division error");
         }
-        System.out.println("No error program continues");
+        System.out.println("Program continues....");
     }
 }
