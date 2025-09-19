c932fe2528f8220775434eba23d5213a53cc1ae6
Shubham Raut <textnow5123@gmail.com>
2025-09-19 15:41:54 +0530

    updated the day3 sql

--- 03.data-colletction-dbms/classfiles/assignment.sql
+++ 03.data-colletction-dbms/classfiles/assignment.sql
@@ -1,7 +1,10 @@
--- Part A – Table Creation
--- Create a table Department with the following fields:
+set sql_safe_updates = 0;
+--
+
+-- Q1. Create a table Department with the following fields:
 -- dept_id (Primary Key)
 -- dept_name
+
 
 CREATE DATABASE assignment1;
 USE assignment1;
@@ -11,7 +14,7 @@ CREATE TABLE departments(
  dept_name varchar(200)
  );
 
--- Create a table Employee with the following fields:
+-- Q2. Create a table Employee with the following fields:
 -- emp_id (Primary Key)
 -- emp_name
 -- salary
@@ -26,6 +29,8 @@ dept_id int,
 foreign key (dept_id) references departments(dept_id)
 );
 
+
+-- part B - data insertions
 
 INSERT INTO departments
 (dept_id, dept_name) VALUES
@@ -46,42 +51,57 @@ INSERT INTO employee
 
 
 
+
+
+
 SELECT * FROM departments;
 select * from employee;
 
-
--- Display all employees with their department names.
+-- Part C 
+-- Q4. Display all employees with their department names.
 SELECT emp_name, dept_name FROM employee em
 JOIN departments dp ON dp.dept_id = em.dept_id
 ;
 
 
--- Find employees who work in the police department.
+-- Q5. Find employees who work in the police department.
 
 SELECT emp_name, dept_name FROM employee em
 JOIN departments dp on dp.dept_id = em.dept_id
 WHERE dp.dept_name LIKE "police";
 
 
--- List department names along with the number of employees in each department.
+-- Q6. List department names along with the number of employees in each department.
 SELECT dept_name, COUNT(emp_name) as no_of_emp FROM employee em
 JOIN departments dp on dp.dept_id = em.dept_id
 GROUP BY dept_name;
 
 
--- Add a new employee 'Meena' in department 'Finance'.
+-- Q7. Add a new employee 'Meena' in department 'Finance'.
 SELECT * FROM departments;
 
 INSERT INTO employee VALUES(6, "Meena",12000,6);
 
 UPDATE employee SET dept_id = 6 WHERE emp_name = "Meena";
 
-set sql_safe_updates = 0;
+
+-- Q8. Try inserting an employee with a non-existing department id (say dept_id = 10). What happens? Why?
+
+INSERT INTO employee (dept_id, emp_name) VALUES (10, "jamin");
+-- it shows error
 
 
-SELECT * FROM departments;
+-- Q9. Delete the Finance department. What happens to employees in that department?
 
+DELETE FROM departments where dept_name = 'Finance';
+
+-- Q10. Modify the Employee table so that if a department is deleted, all its employees are also deleted (ON DELETE CASCADE).
 -- Find employees who work in the IT department.
 SELECT emp_name FROM employee emp
 JOIN departments dp ON emp.dept_id = dp.dept_id
 WHERE dept_name = "IT";
+
+
+-- Delete the Finance department. What happens to employees in that department?
+
+
