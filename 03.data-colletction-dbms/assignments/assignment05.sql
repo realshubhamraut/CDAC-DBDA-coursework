7fef272f69276bd68baa72fb6fd7ad97f219176e
Shubh <117537965+realshubhamraut@users.noreply.github.com>
2025-09-24 09:08:30 +0530

    updated the assignment5

---
+++ 03.data-colletction-dbms/assignments/assignment05.sql
@@ -0,0 +1,33 @@
+create database assignment5;
+use assignment5;
+
+-- 1. Write a SQL statement to change the email column of the employees table with 'not available' for all employees.
+
+
+CREATE table employees(
+email varchar(20),
+
+
+
+
+ 
+-- 2. Write a SQL statement to change the email and commission_pct column of employees
+-- table with 'not available' and 0.10 for all employees.
+-- 3. Write a SQL statement to change the email and commission_pct column of employees
+-- table with 'not available' and 0.10 for those employees whose department_id is 110.
+-- 4. Write a SQL statement to change the email column of employees table with 'not
+-- available' for those employees whose department_id is 80 and gets a commission_pct is
+-- less than .20
+-- 5. Write a SQL statement to change the email column of the employees table with 'not
+-- available' for those employees who belong to the 'Accounting' department.
+-- 6. Write a SQL statement to change the salary of an employee to 8000 whose ID is 105, if
+-- the existing salary is less than 5000.
+-- 7. Write a SQL statement to change the job ID of the employee whose ID is 118, to
+-- SH_CLERK if the employee belongs to the department, whose ID is 30 and the existing
+-- job ID does not start with SH.
+-- 8. Write a SQL statement to increase the salary of employees under the department 40, 90
+-- and 110 according to the company rules that salary will be increased by 25% for
+-- department 40, 15% for department 90 and 10% for department 110 and the rest of the
+-- departments will remain the same.
+-- 9. Write a SQL statement to increase the minimum and maximum salary of PU_CLERK by
+-- 2000 as well as the salary for those employees by 20% and commission percent by 10.
