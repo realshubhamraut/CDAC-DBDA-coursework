58144629624fd7364b2a385fb3e1fee6621bf355
Shubh <117537965+realshubhamraut@users.noreply.github.com>
2025-09-25 16:38:45 +0530

    updated the latest sql code

--- 03.data-colletction-dbms/classfiles/day7.sql
+++ 03.data-colletction-dbms/classfiles/day7.sql
@@ -47,5 +47,54 @@ select date_sub("2024-09-10", interval 1 day);
 
 select date_Add("2023-12-23", interval "1_1" year_month);  -- adds 1 year 1 month
 
+select employee_id, concat(first_name,' ', last_name) as full_name, date_Add(hire_date, interval 30 day) as probation_period from employees;
 
+
+SELECT employee_id, CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE hire_date >= DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY);
+
+
+
+select round(datediff(curdate(), hire_date)/365) as date_diff from employees;
+
+select timestampdiff(month, hire_date, curdate()) from employees;
+select timestampdiff(day, hire_date, curdate()) from employees;
+select timestampdiff(year, hire_date, curdate()) from employees;
+
+SELECT employee_id, CONCAT(first_name, ' ', last_name) AS full_name, hire_date
+from employees
+where timestampdiff(day, hire_date, curdate())<=30;
+
+select date_format("2025-09-25", '%d-%m-%y');
+
+
+
+--  find the employees who servered for 35 + years
+SELECT employee_id, CONCAT(first_name, ' ', last_name) AS full_name from employees
+WHERE timestampdiff(day, hire_date, curdate())>=35;
+
+
+SELECT employee_id, CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE TIMESTAMPDIFF(day, hire_date, CURDATE()) >= 35;
+
+-- find the employees who were hired in the august
+
+SELECT employee_id, CONCAT(first_name, ' ', last_name) AS full_name from employees
+where (month(hire_date) = 8);
+
+
+-- find the employees who are having birth aniversary in next 30 days
+
+select * from employees;
+
+SELECT employee_id, CONCAT(first_name, ' ', last_name) AS full_name from employees
+WHERE (month(hire_date) + interval 1 month);
+
+SELECT employee_id, CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE date_format(hire_date, '%m-%d') between date_format(current_date, '%m-%d') and date_format(date_add(current_date, interval 30 day), '%m-%d');
+select employee_id, concat(first_name, ' ', last_name) as full_name from employees
+where date_format(hire_date, '%m-%d') between date_format(current_date, '%m-%d') and date_format(date_add
 
